91e7306e292191d61df82f8744eb776e3e1a3805
Olukoya Emmanuel <olukoyae01@gmail.com>
2025-08-14 15:28:23 +0100

    allowed host

--- Blog/blog-cms/config/server.ts
+++ Blog/blog-cms/config/server.ts
@@ -1,7 +1,13 @@
-export default ({ env }) => ({
+// config/server.js
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
+  allowedHosts: [
+    'localhost',
+    '127.0.0.1',
+    'devm.westus2.cloudapp.azure.com'
+  ]
 });
